f22967532563308bb6095b9c35d9cc32b9f88ad6
rushsky518 <rushsky158@163.com>
2020-03-25 15:08:50 +0800

    add demo files, for debug

---
+++ pulsar-client/src/main/java/zhang/ConsumerDemo.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements. See the NOTICE
+ * file distributed with this work for additional information regarding copyright ownership. The ASF licenses this file
+ * to You under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package zhang;
+
+import java.util.concurrent.TimeUnit;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.SubscriptionInitialPosition;
+import org.apache.pulsar.client.api.SubscriptionType;
+
+/**
+ * ConsumerDemo
+ */
+public class ConsumerDemo {
+    public static void main(String[] args) throws PulsarClientException {
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://localhost:6650")
+                .build();
+
+        org.apache.pulsar.client.api.Consumer consumer = client.newConsumer()
+                .topic("my-topic")
+                .subscriptionName("my-subscription")
+                .ackTimeout(10, TimeUnit.SECONDS)
+                .subscriptionType(SubscriptionType.Exclusive)
+                .subscriptionInitialPosition(SubscriptionInitialPosition.Earliest)
+                .subscribe();
+        while (true) {
+            // Wait for a message
+            Message msg = consumer.receive();
+
+            try {
+                // Do something with the message
+                System.out.printf("Message received: %s%n", new String(msg.getData()));
+
+                // Acknowledge the message so that it can be deleted by the message broker
+                consumer.acknowledge(msg);
+            } catch (Exception e) {
+                // Message failed to process, redeliver later
+                consumer.negativeAcknowledge(msg);
+            }
+        }
+
+    }
+}
+
